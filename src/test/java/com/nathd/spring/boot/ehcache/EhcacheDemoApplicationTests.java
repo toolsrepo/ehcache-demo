31f7865179f8ee97e7e5c4a8c33ff743dd3247a7
toolsrepo <nath.debasish@gmail.com>
2020-06-21 20:46:02 -0400

    Initial commit

---
+++ src/test/java/com/nathd/spring/boot/ehcache/EhcacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathd.spring.boot.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcacheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
